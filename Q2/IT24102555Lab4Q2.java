d6671fa426f49d4c292e52d84f1f0d5fad338785
IT24102555 <it24102555@my.sliit.lk>
2024-08-20 21:33:47 +0530

    Update IT24102555Lab4Q2.java

--- Q2/IT24102555Lab4Q2.java
+++ Q2/IT24102555Lab4Q2.java
@@ -9,39 +9,32 @@ public class IT24102555Lab4Q2
         System.out.print("Please enter examMarks:");
         double ExamMarks=input.nextDouble();
 
-        
- while(ExamMarks<0||ExamMarks>100)
+if (ExamMarks<0 || ExamMarks>100)
       { 
          System.out.print("Invalid input.");
-         System.out.print("Please enter examMarks:");
-         double examMarks=input.nextDouble();
        }
         
         System.out.print("Please enter Lab submission marks:");
         double labMarks=input.nextDouble();
 
- while(labMarks<0||labMarks>100)
+ if (labMarks<0 || labMarks>100)
       { 
          System.out.print("Invalid input.");
-         System.out.print("Please enter labMarks:");
-         double LabMarks=input.nextDouble();
        }
  
          System.out.print("Enter the percentage taken from the from the examMarks:");
          double ExamPercentage=input.nextDouble();
          System.out.print("Enter the percentage taken from the from the labMarks:");
          double LabPercentage=input.nextDouble();
+      
+ examMarks = examMarks * (exampercentage/100);
+ labMarks = labMarks * (labpercentage/100);
+ double finalMark=examMarks+labMarks;
 
-while(ExamPercentage+LabPercentage!=100)
-      {
-         System.out.print("Invalid input.");
-         System.out.print("Enter the percentage taken from the from the examMarks:");
-         double examPercentage=input.nextDouble();
-         System.out.print("Enter the percentage taken from the from the labMarks:");
-         double labPercentage=input.nextDouble();
-      }
- double finalMark=(ExamMarks*ExamPercentage/100)+(labMarks*LabPercentage/100);
-
+if (finalMarks>100)
+{
+    System.out.print("The percentage must add up to 100");
+}
 System.out.print("The final marks is:"+finalMark);
     
      }
